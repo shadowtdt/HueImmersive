99a31f2f48c7e7e3c6e02c87f1f58956f64c693f
Jannik Szwaczka <blodjer@live.de>
2015-09-11 18:25:02 +0200

    Removed color interface

--- src/hueimmersive/interfaces/IColor.java
+++
@@ -1,9 +0,0 @@
-package hueimmersive.interfaces;
-
-
-public interface IColor
-{
-    void set(float h, float s, float b);
-
-    void getTranslatetColor(boolean useGammaCorrection); // should return data
-}
--- src/hueimmersive/interfaces/ILight.java
+++ src/hueimmersive/interfaces/ILight.java
@@ -1,16 +1,18 @@
 package hueimmersive.interfaces;
 
+import java.awt.Color;
+
 
 public interface ILight
 {
     boolean isOn() throws Exception;
-
     void setOn(boolean on);
+
     void turnOn();
     void turnOff();
 
-    void setColor(IColor color);
-    IColor getColor();
+    void setColor(Color color);
+    Color getColor();
 
     void storeColor();
     void restoreColor();
